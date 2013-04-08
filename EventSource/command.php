90ec079cb20d0a0b7bd493a1161d684c1562f360
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2013-04-08 18:23:27 +0900

    wget で得たファイルのサイズがゼロになる現象の原因を探るためのテストコード

---
+++ EventSource/command.php
@@ -0,0 +1,27 @@
+<?php
+/*
+    Command launcher.
+    Copyright (c) 2013 Naoto Yoshioka
+*/
+$cmd = $_REQUEST['command'];
+
+    putenv("PATH=/usr/local/bin:" . getenv("PATH")); // for 'wget' on MacPorts
+    putenv("DYLD_LIBRARY_PATH="); // disable /Applications/MAMP/Library/bin/envvars
+
+    header("Content-type: text/event-stream; charset=utf-8");
+    header('Cache-Control: no-cache');
+
+    echo "retry: 0\n";
+    flush();
+
+    //chdir("..");
+    $proc = popen($cmd . ' 2>&1', 'r');
+    while ($s = fgets($proc)) {
+        //$s = htmlspecialchars($s); // use this line to produce HTML elem.
+        echo "data: $s\n";
+        flush();
+    }
+    pclose($proc);
+
+    echo "event: finished\n";
+    flush();
