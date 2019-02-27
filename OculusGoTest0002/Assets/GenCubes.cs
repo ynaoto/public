b42266627de93f670e63b1334e5d4ff2b57888f0
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2019-02-27 16:06:27 +0900

    copied from 001

---
+++ OculusGoTest0002/Assets/GenCubes.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenCubes : MonoBehaviour
+{
+    public Transform prefab;
+    public Text text;
+    List<Transform> instances = new List<Transform>();
+
+    void addCube()
+    {
+        var r = 3.0f;
+        var x = Random.Range(-r, r);
+        var y = Random.Range(-r, r);
+        var z = 0.0f;
+        var o = GameObject.Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity, transform);
+        instances.Add(o);
+    }
+
+    void delCube()
+    {
+        var i = instances.Count - 1;
+        if (0 <= i)
+        {
+            var o = instances[i];
+            Destroy(o.gameObject);
+            instances.RemoveAt(i);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dt = Time.deltaTime;
+        if (1000 * dt < 32)
+        {
+            addCube();
+        }
+        else
+        {
+            delCube();
+        }
+
+        string s = "";
+        var ql = QualitySettings.GetQualityLevel();
+        s += $"Quality: {QualitySettings.names[ql]}";
+        s += "\n";
+        s += $"{1000*dt:F1} ms; ";
+        s += $"{instances.Count} instances";
+        text.text = s;
+    }
+}
---
+++ OculusGoTest0002/Assets/Rot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = transform.position.x;
+        var y = transform.position.y;
+        var z = transform.position.z;
+        transform.Rotate(1.0f*x, 1.0f*y, 1.0f*z);
+    }
+}
