c7b4ea453539c95ee971f191c0ede8db21ffc204
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2019-03-06 18:13:42 +0900

    performance test

--- OculusGoTest0002/Assets/GenCubes.cs
+++ OculusGoTest0002/Assets/GenCubes.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿//#define NO_DESTROY
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Profiling;
 
 public class GenCubes : MonoBehaviour
 {
@@ -9,33 +12,66 @@ public class GenCubes : MonoBehaviour
     public Text text;
     public Transform controllerIcon;
     public Transform goalIcon;
-    List<Transform> instances = new List<Transform>();
+    List<Transform> instances;
+#if NO_DESTROY
+    int numActivated;
+#endif
+
+    void initCubes()
+    {
+        instances = new List<Transform>(1000);  // HERE: ガバッと取っておく。ちょっと性能に効いてる？
+    #if NO_DESTROY
+        for (var i = 0; i < 1000; i++) {
+            var r = 3.0f;
+            var x = Random.Range(-r, r);
+            var y = Random.Range(-r, r);
+            var z = 0.0f;
+            var o = GameObject.Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity, transform);
+            o.gameObject.SetActive(false);
+            instances.Add(o);
+        }
+        numActivated = 0;
+    #endif
+    }
 
     void addCube()
     {
+    #if NO_DESTROY
+        var o = instances[numActivated];
+        o.gameObject.SetActive(true);
+        numActivated++;
+    #else
         var r = 3.0f;
         var x = Random.Range(-r, r);
         var y = Random.Range(-r, r);
         var z = 0.0f;
         var o = GameObject.Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity, transform);
         instances.Add(o);
+    #endif
     }
 
     void delCube()
     {
-        var i = instances.Count - 1;
-        if (0 <= i)
+    #if NO_DESTROY
+        if (0 <= numActivated)
         {
-            var o = instances[i];
-            Destroy(o.gameObject);
-            instances.RemoveAt(i);
+            var o = instances[numActivated - 1];
+            o.gameObject.SetActive(false);
         }
+    #else
+        if (0 <= instances.Count)
+        {
+            var o = instances[0];
+            Destroy(o.gameObject);
+            instances.RemoveAt(0);
+        }
+    #endif
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        initCubes();
     }
 
     #if UNITY_EDITOR
@@ -70,32 +106,48 @@ public class GenCubes : MonoBehaviour
         RaycastHit hit;
         Vector3 dir = controllerIcon.TransformDirection(Vector3.forward);
         int layerMask = 1 << 31;
-        if (Physics.Raycast(controllerIcon.position, dir, out hit, Mathf.Infinity, layerMask)) {
+
+        // 以下の違いで測定できるような性能差は出ていない
+        var maxDistance = Mathf.Infinity;
+        //var maxDistance = 10.0f;
+
+        if (Physics.Raycast(controllerIcon.position, dir, out hit, maxDistance, layerMask)) {
             goalIcon.position = hit.point;
         }
 
         var dt = Time.deltaTime;
-        if (1000 * dt < 32)
+        if (dt < 1.0f/30)  // まだ余裕
         {
             addCube();
         }
-        else
+        else if (1.0f/15 < dt)  // ちょっときつすぎる
         {
             delCube();
         }
 
-        var lastP = goalIcon.position;
-        foreach (var o in instances) {
-            o.position += 0.1f*(lastP - o.position);
-            lastP = o.position;
+        var goal = goalIcon.position;
+    #if NO_DESTROY
+        for (var i = 0; i < numActivated; i++) {
+            var o = instances[i];
+            o.position = Vector3.Lerp(o.position, goal, 0.1f);
+            goal = o.position;
         }
+    #else
+        foreach (var o in instances) {
+            o.position = Vector3.Lerp(o.position, goal, 0.1f);
+            goal = o.position;
+        }
+    #endif
 
-        string s = "";
         var ql = QualitySettings.GetQualityLevel();
-        s += $"Quality: {QualitySettings.names[ql]}";
-        s += "\n";
-        s += $"{1000*dt:F1} ms; ";
-        s += $"{instances.Count} instances";
-        text.text = s;
+    #if NO_DESTROY
+        text.text = $"Quality: {QualitySettings.names[ql]}\n"
+            + $"{1000 * dt:F1} ms; "
+            + $"{numActivated} activated";
+    #else
+        text.text = $"Quality: {QualitySettings.names[ql]}\n"
+            + $"{1000 * dt:F1} ms; "
+            + $"{instances.Count} instances";
+    #endif
     }
 }
