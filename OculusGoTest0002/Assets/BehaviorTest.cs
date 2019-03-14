522c8ea2b9f94b01747ec486c9448319cbe456de
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2019-03-14 13:28:52 +0900

    added sphere for ref, and copied crow prefab

---
+++ OculusGoTest0002/Assets/BehaviorTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log($"{this.name}: Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log($"{this.name}: Update");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log($"{this.name}: OnTriggerEnter");
+    }
+}
