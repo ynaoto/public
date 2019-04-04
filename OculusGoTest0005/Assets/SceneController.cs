ad584dc124d1d3cf7585d3f1eebf724824f9e4ed
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2019-04-04 17:46:55 +0900

    Scene change with Trigger button

---
+++ OculusGoTest0005/Assets/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public string nextSceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var controller = OVRInput.GetActiveController();
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, controller)) {
+            SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+        }
+    }
+}
