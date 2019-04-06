b9dafd93a9e2d1b7e6a73f5a5010ceddb832a10c
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2019-04-07 01:08:45 +0900

    camera control start working

--- OculusGoTest0005/Assets/SceneController.cs
+++ OculusGoTest0005/Assets/SceneController.cs
@@ -6,19 +6,33 @@ using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
     public string nextSceneName;
+    public Camera camera;
+    GameObject cameraHolder;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraHolder = new GameObject("cameraHolder");
+        camera.transform.SetParent(cameraHolder.transform);
+        cameraHolder.transform.SetParent(transform);
     }
 
     // Update is called once per frame
     void Update()
     {
         var controller = OVRInput.GetActiveController();
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, controller)) {
-            SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+        if (controller != OVRInput.Controller.None)
+        {
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+            }
+            if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
+            {
+                var v = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+                cameraHolder.transform.Translate(0.0f, 0.0f, -0.1f*v.y);
+                Debug.Log($"v.y = {v.y}; camera at {camera.transform.position}");
+            }
         }
     }
 }
