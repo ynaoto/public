202abe65b7e03f67dcdcfbbb1aa1dda9742d8fa4
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2019-05-10 02:09:17 +0900

    worked

---
+++ ARTest0001/Assets/ARTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARTest : MonoBehaviour
+{
+    void faceAdded(ARFaceAddedEventArgs args)
+    {
+        Debug.Log("**** faceAdded");
+    }
+
+    void planeAdded(ARPlaneAddedEventArgs args)
+    {
+        Debug.Log("**** planeAdded");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var xxx = GetComponent<ARFaceManager>();
+        xxx.faceAdded += faceAdded;
+
+        var yyy = GetComponent<ARPlaneManager>();
+        yyy.planeAdded += planeAdded;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
