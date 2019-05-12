d96c5cd0d8aefde2487da9733ff0a873a46b6444
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2019-05-13 02:40:14 +0900

    Unity-chan has come

--- ARTest0001/Assets/ARTest.cs
+++ ARTest0001/Assets/ARTest.cs
@@ -5,24 +5,24 @@ using UnityEngine.XR.ARFoundation;
 
 public class ARTest : MonoBehaviour
 {
-    void faceAdded(ARFaceAddedEventArgs args)
-    {
-        Debug.Log("**** faceAdded");
-    }
+    [SerializeField]
+    GameObject prefab;
+    bool virgin = true;
 
     void planeAdded(ARPlaneAddedEventArgs args)
     {
-        Debug.Log("**** planeAdded");
+        if (virgin)
+        {
+            Instantiate(prefab, args.plane.transform);
+            virgin = false;
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        var xxx = GetComponent<ARFaceManager>();
-        xxx.faceAdded += faceAdded;
-
-        var yyy = GetComponent<ARPlaneManager>();
-        yyy.planeAdded += planeAdded;
+        var planeManager = GetComponent<ARPlaneManager>();
+        planeManager.planeAdded += planeAdded;
     }
 
     // Update is called once per frame
