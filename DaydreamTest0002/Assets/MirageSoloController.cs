72cbeec8c3e3ec19c5b5faddf72ebf5feabf3f80
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2019-05-16 17:27:05 +0900

    support controller

---
+++ DaydreamTest0002/Assets/MirageSoloController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirageSoloController : MonoBehaviour
+{
+    GvrControllerInputDevice gvrControllerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gvrControllerInput = GvrControllerInput.GetDevice(GvrControllerHand.Dominant);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrControllerInput.GetButton(GvrControllerButton.TouchPadTouch))
+        {
+            var pos = gvrControllerInput.TouchPos;
+            var maxv = 300.0f * 1000f / 60f / 60f; // m/s
+            transform.Translate(maxv * pos.y * Time.deltaTime * Camera.main.transform.forward);
+        }
+    }
+}
