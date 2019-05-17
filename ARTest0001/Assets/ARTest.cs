c0c466d2bbe95079b6e7ba51158b707c7a999baf
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2019-05-17 15:28:42 +0900

    light

--- ARTest0001/Assets/ARTest.cs
+++ ARTest0001/Assets/ARTest.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityChan;
 
+[RequireComponent(typeof(AudioSource))]
 public class ARTest : MonoBehaviour
 {
     [SerializeField]
     GameObject prefab;
+    [SerializeField]
+    Light light;
     [SerializeField]
     Transform defaultOrigin;
     bool virgin = true;
     GameObject instance;
     Animator animator;
 
-
     void goUnityChan(Transform parent)
     {
         instance = Instantiate(prefab, parent);
         var musicStarter = instance.GetComponent<MusicStarter>();
         musicStarter.refAudioSource = GetComponent<AudioSource>();
-        animator = instance.GetComponent<Animator>();
     }
 
     void planeChanged(ARPlanesChangedEventArgs args)
@@ -32,12 +33,35 @@ public class ARTest : MonoBehaviour
         }
     }
 
+    void cameraFrameReceived(ARCameraFrameEventArgs args)
+    {
+        if (light != null)
+        {
+            var le = args.lightEstimation;
+            if (le.averageBrightness.HasValue)
+            {
+                light.intensity = le.averageBrightness.Value;
+            }
+            if (le.averageColorTemperature.HasValue)
+            {
+                light.colorTemperature = le.averageColorTemperature.Value;
+            }
+            if (le.colorCorrection.HasValue)
+            {
+                light.color = le.colorCorrection.Value;
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         var planeManager = FindObjectOfType<ARPlaneManager>();
-        //var planeManager = GetComponent<ARPlaneManager>();
         planeManager.planesChanged += planeChanged;
+
+        var cameraManager = FindObjectOfType<ARCameraManager>();
+        cameraManager.frameReceived += cameraFrameReceived;
+
 #if UNITY_EDITOR
         goUnityChan(defaultOrigin);
 #endif
@@ -46,8 +70,12 @@ public class ARTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (animator != null)
+        if (instance != null)
         {
+            if (animator == null)
+            {
+                animator = instance.GetComponent<Animator>();
+            }
             var stateInfo = animator.GetCurrentAnimatorStateInfo(0);
             if (1.0f <= stateInfo.normalizedTime)
             {
